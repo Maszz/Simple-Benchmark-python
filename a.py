be4528654c064e3ac054435838d339caaad8e059
Masz <mawin38408@gmail.com>
2021-12-15 23:43:44 +0700

    add configfile and implemet api for frontend

--- a.py
+++
@@ -1,7 +0,0 @@
-# import configparser
-# config = configparser.ConfigParser()
-# config.read_file(open('config.cfg'))
-# a = config.get('I/O_BENCHMARK', 'FILE_SIZE')
-
-# print(eval(a))
-print(len([[]]))
